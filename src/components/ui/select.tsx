bb7758cfb973067a8e76c6b7f55b7e59d86199eb
davidjp03 <70982174+davidjp03@users.noreply.github.com>
2025-08-24 23:18:01 -0500

    Replace empty interfaces with type aliases

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,10 +1,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface SelectProps
-  extends React.SelectHTMLAttributes<HTMLSelectElement> {
-  // Extending HTMLSelectElement attributes
-}
+export type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement>
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
   ({ className, children, ...props }, ref) => {
