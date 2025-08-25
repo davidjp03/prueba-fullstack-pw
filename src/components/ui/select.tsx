b0328c173ffe66e053d6b42eea3c53d095559b26
davidjp03 <70982174+davidjp03@users.noreply.github.com>
2025-08-24 23:13:28 -0500

    Fix remaining TypeScript interface errors

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -2,7 +2,9 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface SelectProps
-  extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+  extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  // Extending HTMLSelectElement attributes
+}
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
   ({ className, children, ...props }, ref) => {
